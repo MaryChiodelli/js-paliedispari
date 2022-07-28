1c3bba34d1d7873f00bb57cf144a7bf71c980b2e
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-28 16:23:28 +0200

    add random number and sum

--- js/app.js
+++ js/app.js
@@ -1,25 +1,54 @@
-// chiedere all’utente di inserire una parola
-const userWord = prompt('Inserisci una parola');
+// // PALINDROMA
 
-// controllare se la parola inserita è palindroma (funzione)
-function isPalindroma(text) {
-    // dividere la parola in lettere
-    const chars = text.split('');
-    const revertChars = [];
-    // invertire l'ordine delle lettere
-    for (let i = chars.length - 1; i >= 0; i--) {
-        const char = chars[i];
-        revertChars.push(char);
-    }
-    console.log(chars, revertChars);
+// // chiedere all’utente di inserire una parola
+// const userWord = prompt('Inserisci una parola');
 
-    // controllare che la parola sia palindroma
-    let isPalindroma = false;
-    if (chars.join() === revertChars.join()) {
-        isPalindroma = true;
-    }
+// // controllare se la parola inserita è palindroma (funzione)
+// function isPalindroma(text) {
+//     // dividere la parola in lettere
+//     const chars = text.split('');
+//     const revertChars = [];
+//     // invertire l'ordine delle lettere
+//     for (let i = chars.length - 1; i >= 0; i--) {
+//         const char = chars[i];
+//         revertChars.push(char);
+//     }
+//     console.log(chars, revertChars);
+
+//     // controllare che la parola sia palindroma
+//     let isPalindroma = false;
+//     if (chars.join() === revertChars.join()) {
+//         isPalindroma = true;
+//     }
     
-    return isPalindroma;
-}
+//     return isPalindroma;
+// }
 
-console.log(isPalindroma(userWord)); 
+// console.log(isPalindroma(userWord));
+
+
+
+
+// PARI E DISPARI
+
+// chiedere all'utente di scegliere pari o dispari
+// chiedere all'utente di inserire un numero da 1 a 5
+const evenOrOdd = prompt('Pari o dispari?');
+const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+// generare un numero random da 1 a 5 per il computer
+const pcNumber = getRandomInt();
+console.log(evenOrOdd, userNumber, pcNumber);
+
+// sommare i due numeri
+const sum = userNumber + pcNumber;
+
+// stabilire se la somma dei due numeri è pari o dispari
+
+
+// Dichiariamo chi ha vinto.
+
+function getRandomInt() {
+    const num = Math.floor(Math.random() * 5) + 1;
+    return num;
+}
