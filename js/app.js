77df21951255f7f598421bb2c3bcf3e6312b9db5
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-28 15:37:41 +0200

    revert chars order

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// chiedere all’utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+
+// controllare se la parola inserita è palindroma (funzione)
+function isPalindroma(text) {
+    // dividere la parola in lettere
+    const chars = text.split('');
+    const revertChars = [];
+    // invertire l'ordine delle lettere
+    for (let i = chars.length - 1; i >= 0; i--) {
+        const char = chars[i];
+        revertChars.push(char);
+    }
+    console.log(chars, revertChars);
+    // controllare che la parola sia palindroma
+}
+
+console.log(isPalindroma(userWord));
